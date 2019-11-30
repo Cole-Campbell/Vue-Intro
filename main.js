974589dedbf4a544e2c68db2513c84cbc7a74f28
Cole Campbell <cole.william.campbell@gmail.com>
2019-11-30 19:07:44 +0000

    Iteration
    Added iteration of object variants and sizes.

--- main.js
+++ main.js
@@ -6,7 +6,24 @@ var app = new Vue({
       description: "A singular sock, nothing more",
       imageURL: "http://placehold.it/250x300",
       inStock: true,
-      onSale: true
+      onSale: true,
+      details: ["80% Cotton", "20% Silk", "Single Sock"],
+      variants: [
+        {
+          id: 1234,
+          color: "green"
+        },
+        {
+          id: 1235,
+          color: "blue"
+        }
+      ],
+      sizes: [
+        { name: "small", available: true },
+        { name: "medium", available: false },
+        { name: "large", available: true },
+        { name: "x-large", available: true }
+      ]
     }
   }
 });
